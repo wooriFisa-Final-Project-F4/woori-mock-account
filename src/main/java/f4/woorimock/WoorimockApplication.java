6b9b279b800a81b99102011e6548aec29bf29742
endlessmomo <soohyuk96@naver.com>
2023-08-22 17:14:28 +0900

    [#1][build] : Environment Settings

---
+++ src/main/java/f4/woorimock/WoorimockApplication.java
@@ -0,0 +1,13 @@
+package f4.woorimock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorimockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoorimockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f4/woorimock/WoorimockApplicationTests.java
@@ -0,0 +1,13 @@
+package f4.woorimock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorimockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
