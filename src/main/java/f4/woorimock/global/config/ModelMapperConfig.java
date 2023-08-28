134c72c8a2c7d4a320f2e79f0fd179e19cd79628
endlessmomo <soohyuk96@naver.com>
2023-08-28 20:31:07 +0900

    [#4][build] : entity -> dto mapping 작업을 위한 ModelMapper 의존성 등록 및 빈 주입

---
+++ src/main/java/f4/woorimock/global/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package f4.woorimock.global.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
